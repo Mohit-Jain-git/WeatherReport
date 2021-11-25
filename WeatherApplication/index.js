6da4328ac1ffc4cbf355bc76148df73016be68fc
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-11-25 13:12:58 +0530

    Weather Report

---
+++ WeatherApplication/index.js
@@ -0,0 +1,44 @@
+let weather = {
+  apiKey: "0ae33063a06f72754a500dce295c0cef",
+  fetchWeather: function (city) {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").innerText;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".speed").innerText =
+      "Wind Speed: " + speed + " km/h";
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/user/erondu/1600x900?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+weather.fetchWeather("India");
